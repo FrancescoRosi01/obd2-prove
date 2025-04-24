c39c81eeb19fa1a51650ad58357b711ed4c9a17c
FrancescoRosi01 <139993451+FrancescoRosi01@users.noreply.github.com>
2025-04-24 17:56:56 +0200

    Update prova.py

--- prova.py
+++ prova.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dbc_filename = "11-bit-OBD2-v4.0.dbc"
-mf4_filename = r"11-bit-obd2.MF4"
+mf4_filename = r"00000001.MF4"
 mdf = MDF(mf4_filename)
 # dalla documentazione
 # The valid bus (il secondo elem della tupla) is an integer specifying for
@@ -12,8 +12,9 @@ db_files = {"CAN": [(dbc_filename, 0)]}
 # interpreta i dati usando il file dbc
 mdf_scaled = mdf.extract_bus_logging(db_files)
 df = mdf_scaled.to_dataframe()
+print(df.columns)
 # estrai la velocità
-column_name = "S01PID0D_VehicleSpeed"
+column_name = "S01PID0C_EngineRPM"
 if column_name in df.columns:
     plt.figure(figsize=(10, 6))
     plt.plot(df.index, df[column_name], label=column_name)
@@ -26,14 +27,4 @@ if column_name in df.columns:
 else:
     print(f"Column '{column_name}' not found in the DataFrame.")
 
-# esporta in csv
-mdf_scaled.export(
-    "csv",
-    filename="phys-val",
-    time_as_date=True,
-    single_time_base=True,
-    escapechar="\\", 
-    quotechar='"',     
-    add_units = True
-)
 
